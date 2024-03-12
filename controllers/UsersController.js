5a2427a121a17d8cc876449212b79dfd9b5eedd9
manucho254 <manuchoadero@gmail.com>
2024-03-12 13:29:41 +0000

    create user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,17 +1,16 @@
 import dbClient from '../utils/db';
 import { hashPassword } from '../utils/helpers';
 
-const postNew = (req, res) => {
-  console.log(req);
+const postNew = async (req, res) => {
   const { email, password } = req.body;
-  let user = dbClient.findUser(email);
+  let user = await dbClient.findUser(email);
 
-  if (!email) res.status(400).json({ error: 'Missing email' });
-  if (!password) res.status(400).json({ error: 'Missing password' });
-  if (user) res.status(400).json({ error: 'Already exist' });
+  if (!email) return res.status(400).json({ error: 'Missing email' });
+  if (!password) return res.status(400).json({ error: 'Missing password' });
+  if (user) return res.status(400).json({ error: 'Already exist' });
 
-  user = dbClient.addUser(email, hashPassword(password));
-  res.status(200).json({ id: user._id, email: user.email });
+  user = await dbClient.addUser(email, hashPassword(password));
+  return res.status(200).json({ id: user._id, email: user.email });
 };
 
 module.exports = { postNew };
