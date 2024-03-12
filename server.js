7f760d61619788f150509c81a1d2ddf6203b8970
Emmanuel manucho <manuchoadero@gmail.com>
2024-03-12 16:26:04 +0300

    Create a new user

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import appRoutes from './routes/index';
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use('/', appRoutes);
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
