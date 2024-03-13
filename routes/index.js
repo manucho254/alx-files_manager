793f1fb88322e73d72e32dd858b1a6b9ffc3671d
Emmanuel manucho <manuchoadero@gmail.com>
2024-03-13 17:11:11 +0300

    fixes

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,13 @@ const { postUpload, getShow, getIndex } = require('../controllers/FilesControlle
 
 const router = express.Router();
 
-router.get('/status', getStatus);
-router.get('/stats', getStats);
-router.post('/users', postNew);
-router.get('/connect', getConnect);
-router.get('/disconnect', getDisconnect);
-router.get('/users/me', getMe);
-router.post('/files', postUpload);
-router.get('/files/:id', getShow);
-router.get('files', getIndex);
+router.route('/status').get(getStatus);
+router.route('/stats').get(getStats);
+router.route('/connect').get(getConnect);
+router.route('/disconnect').get(getDisconnect);
+router.route('/users/me').get(getMe);
+router.route('files').get(getIndex).post(postUpload);
+router.route('/files/:id').get(getShow);
+router.route('/users').post(postNew);
 
 module.exports = router;
