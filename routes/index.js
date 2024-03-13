27a2f78ccc39618ebf67643bca5e4ea79a6d839a
Emmanuel manucho <manuchoadero@gmail.com>
2024-03-13 20:15:28 +0300

    fixes

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.route('/stats').get(getStats);
 router.route('/connect').get(getConnect);
 router.route('/disconnect').get(getDisconnect);
 router.route('/users/me').get(getMe);
-router.route('files').get(getIndex).post(postUpload);
+router.route('/files').get(getIndex).post(postUpload);
 router.route('/files/:id').get(getShow);
 router.route('/users').post(postNew);
 
