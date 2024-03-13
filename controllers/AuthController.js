59ec75fb37d2a8ab9ba3906e4602897772374df7
manucho254 <manuchoadero@gmail.com>
2024-03-13 18:16:25 +0000

    fixes

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -6,26 +6,24 @@ import { hashPassword } from '../utils/helpers';
 
 const getConnect = async (req, res) => {
   const authHeader = req.headers.authorization;
+  try {
+    // verify auth credentials
+    const base64Credentials = authHeader.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [email, password] = credentials.split(':');
+    const hashedPass = hashPassword(password);
+    const user = await dbClient.findUser({ email, password: hashedPass });
+    const token = uuidv4();
 
-  if (!authHeader || !authHeader.startsWith('Basic ')) {
+    if (!user) {
+       return res.status(401).json({ error: 'Unauthorized' });
+    }
+  
+    await redisClient.set(`auth_${token}`, user._id.toString(), 86400);
+    return res.status(200).json({ token });
+  } catch (err) {
     return res.status(401).json({ error: 'Unauthorized' });
   }
-
-  // verify auth credentials
-  const base64Credentials = authHeader.split(' ')[1];
-  const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
-  const [email, password] = credentials.split(':');
-  const hashedPass = hashPassword(password);
-  const user = await dbClient.findUser({ email, password: hashedPass });
-  const token = uuidv4();
-
-  if (!user) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-
-  console.log(user);
-  await redisClient.set(`auth_${token}`, user._id.toString(), 86400);
-  return res.status(200).json({ token });
 };
 
 const getDisconnect = async (req, res) => {
