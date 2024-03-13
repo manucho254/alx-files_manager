d54fe7313f3a085a7c738dc95c737faccced6230
Emmanuel manucho <manuchoadero@gmail.com>
2024-03-13 17:13:05 +0300

    fixes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use('/', appRoutes);
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
